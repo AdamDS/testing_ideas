0611a66e1e584efb3494aa99f8a53054ba4db3ab
Adam D Scott <adam@adamscottphd.com>
2014-04-28 17:50:43 -0500

    Update factorial.cpp
    Added factorial_i (returns int), and changed the pointer function to factorial_p. Also added while loop in main.

--- factorial.cpp
+++ factorial.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 //function prototypes
-void factorial(int*n,int *m);
 int mult(int a, int b);
+void factorial_p(int*,int*);
+int factorial_i(int);
 
 //Example: 4! = 4*3*2*1 = 24
 //*n = 1  *m = 4    cout--1 4
@@ -12,22 +13,37 @@ int mult(int a, int b);
 //*n = 24 *m = 1    cout--24 1
 
 int main(void) {
-  int m = 4;
-  int *n;
-  n = new int;
-  *n = 1;
-  factorial(&n,&m); //passing addresses
-  cout << m << "!" << n << endl;
-  
+  int m = 0;
+  int n, o;
+  while(m>=0) {
+    cout << "Enter natural number (<0 exits): ";
+    cin >> m;
+    n = 1;
+    o = m;
+    cout << n << " " << m << endl;
+    factorial(&n,&m); //passing addresses
+    cout << o << "! = " << n << endl;
+  }
   return 0;
 }
 
 int mult(int a, int b) {  return a*b; }
 
-void factorial(int *n,int *m) {
-  if(*m>0) {  //if incrementer goes to zero
-    *m--;
-    cout << *n << " " << *m << endl;
-    factorial(&mult(*n,*m),&m);
+void factorial_p(int *c,int *d) {
+  if(*d>0) {  //if incrementer goes to zero
+    cout << *c << " " << *d << endl;
+    *c = mult(*c,*d);
+    (*d)--;
+    factorial(c,d);
+  }
+}
+
+int factorial_i(int e) {
+  if(e>0) {
+    cout << e << endl;
+    return e*factorial_i(e-1);
+  } else {  
+    cout << "base" << endl;
+    return 1; 
   }
 }
