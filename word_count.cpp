785d2fb712a417dd1f177a70c302466a7f832102
Adam D Scott <adam@adamscottphd.com>
2014-05-01 14:59:43 -0500

    Create word_count.cpp
    This counts the number of words in "test_read.in". All alphabet characters are set to lower case, and any end punctuation to a word is removed.

---
+++ word_count.cpp
@@ -0,0 +1,87 @@
+#include <iostream> //cin, cout
+#include <fstream> //ifstream, open, is_open, close, getline, 
+#include <cctype> //isalnum, isspace, isupper, tolower
+using namespace std;
+
+//prototype functions
+char check_case(char);
+void clean_word(string*);
+
+int main() {
+  /*string str_filename;
+  cout << "Enter the filename to read. ";
+  cin >> str_filename;
+  //convert string to char array, needed for ifstream open
+  char ch_filename[str_filename.size()];
+  for (int i=0; i<str_filename.size(); i++) ch_filename[i] = str_filename[i];
+  cout << "Trying file " << ch_filename << endl;
+  ifstream file_in(ch_filename);*/
+  ifstream file_in("test_read.in");  
+  
+  if (file_in==NULL)  {
+    perror ("Error opening file");
+  } else {
+    string line_in; //stores each line of the file
+    string word("");
+    char achar; //current char
+    char pause;
+    int num_words(0); //counts total number of words
+    int num_lines(0);
+
+    while(getline(file_in,line_in)) { //file_in line to line_in
+      num_lines++;
+      word = "";  //make sure word resets
+      cout << endl << line_in << endl;  //print line read in
+      for(int i=0; i<line_in.size(); i++) { //cycle through line
+        achar = line_in[i];
+        if (isspace((int)achar)) {  //achar is space or end line
+//          cout << word << endl;
+          clean_word(&word);
+          cout << word;
+          //to nodes
+            num_words++;  //FOR LATER: check nodes for same word
+          word = "";  //reset word
+          cout << "\t\tThere are " << num_words << " words now." << endl << endl;
+        } else {  //achar not space
+          achar = check_case(achar);
+          word += achar;
+//          cout << word << " didn't find space" << endl;
+        }
+      } //for line_in
+      if (word.size()>0) {  //reached end of line, if word has size, need to record
+//        cout << word << " last word of line" << endl;
+//        cout << word << endl;
+        clean_word(&word);
+        cout << word;
+        //to nodes
+          num_words++;  //FOR LATER: check nodes for same word
+        word = "";  //reset word
+      }
+      cout << "\t\tThere are " << num_words << " words after " << num_lines << " lines." << endl << endl;
+//      cin.get(pause);
+//      cout << "\tHIT ENTER TO CONTINUE" << endl << endl;
+    } //while getline
+    cout << endl;
+    file_in.close();
+  }
+  return 0;
+}
+
+char check_case(char a) {
+  if (isupper((int)a)) 
+    a = (char)tolower((int)a);
+  return a;
+}
+
+void clean_word(string* b) {
+  string::reverse_iterator rit = b->rbegin(); //start from end of word
+  int range2delete = 0;
+  while (!isalnum((int)(*rit))) { //while char is not alphanumeric
+    range2delete++;
+//    cout << "\tCHECKING END VALUES OF WORD " << *rit << endl;
+    ++rit;
+  }
+//  cout << "\tWORD BEFORE " << range2delete << " DELETIONS " << *b << endl;
+  b->erase(b->end()-range2delete,b->end());
+//  cout << "\tWORD AFTER ANY DELETION " << *b << endl;
+}
